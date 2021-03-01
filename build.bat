a257cd5b765d40509969df839e5b7369e1749f1e
Massimo Grattieri <massimo.grattieri@boschrexroth.it>
2021-03-01 13:53:40 +0100

    Initial Commit

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off 
-:: Ver 1.0
+:: Ver 1.1
 
 :: Fetch data that will be used to build documentation
 git clone https://github.com/rexroth-unofficial-ita/doc-build.git
@@ -20,23 +20,32 @@ powershell Expand-Archive -LiteralPath "pandoc.zip" -DestinationPath "../pandoc"
 pandoc.exe ../../README.md  --template ../../doc-build/template/template.html --toc -B ../../doc-build/template/nav.html --metadata-file ../../APPLICATION-DATA.yalm -o ../../GUIDA.html
 cd ../../
 
-:: This part will move README.md to parent folder after having corrected images path.
-setlocal enableextensions disabledelayedexpansion
-for %%a in ("%~dp0\.") do set "parent=%%~nxa"
-for /f "delims=" %%i in ('type "README.md" ^& break ^> "README.md" ') do (
-    set "line=%%i"
-	setlocal enabledelayedexpansion
-    >>"../README.md" echo(!line:assets=%parent%/assets!
-    endlocal
-)
-		
+
+::setlocal
+::SET /P AREYOUSURE=Move README.md to parent folder ([Y]/N)?
+::IF /I "%AREYOUSURE%" EQU "N" GOTO END
+	:: This part will move README.md to parent folder after having corrected images path.
+	setlocal enableextensions disabledelayedexpansion
+	for %%a in ("%~dp0\.") do set "parent=%%~nxa"
+	for /f "delims=" %%i in ('type "README.md" ^& break ^> "README.md" ') do (
+		set "line=%%i"
+		setlocal enabledelayedexpansion
+		>>"../README.md" echo(!line:assets=%parent%/assets!
+		endlocal
+	)
+	del /F README.md
+:::END
+
+::setlocal
+::SET /P AREYOUSURE=Archive readme files (Y/[N])?
+::IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
+	:: Archive md files and delete them
+	powershell Compress-Archive "*.md" -DestinationPath "backup.zip"
+	del *.md
+:::END
+
+				
 :: Remove files used for building
-del /F README.md
 rd /s /q doc-build
 rd /s /q .git
 dir
-
-:: Archive md files and delete them
-powershell Compress-Archive "*.md" -DestinationPath "backup.zip"
-del *.md
-
